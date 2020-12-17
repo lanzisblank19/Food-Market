169db6c546a50b8eb996f34933a7e6e408ba9b56
Lanz <lanz.blank05@gmail.com>
2020-12-17 18:15:13 +0800

    others and store

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -10,6 +10,9 @@ export class StoreComponent implements OnInit {
   myimage2:string = "assets/image/sampleslide.jpg";
   myimage3:string = "assets/image/menu.jpg";
   myimage4:string = "assets/image/burgerfries.jpg";
+  myimage5:string = "assets/image/fb.png";
+  myimage6:string = "assets/image/ig.png";
+  myimage7:string = "assets/image/twitter.png";
   constructor() { }
 
   ngOnInit(): void {
